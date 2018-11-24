af78f67da13027c39b8272e052a7224a2038e94a
wzt1229 <wzt1229@126.com>
2018-11-24 23:38:20 +0800

    first

---
+++ iOSOpenCVDemo/iOSOpenCVDemo/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  iOSOpenCVDemo
+//
+//  Created by wztMac on 2018/11/24.
+//  Copyright © 2018年 wztMac. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
